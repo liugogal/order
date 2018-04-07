34a82bd9b467854fdd248395e7d4caa0dad3afc5
liugogal <259394345@qq.com>
2018-04-07 21:46:58 +0800

    update: ordercontroller的补充

--- src/main/java/com/huatuo/order/controller/OrderController.java
+++ src/main/java/com/huatuo/order/controller/OrderController.java
@@ -1,11 +1,15 @@
 package com.huatuo.order.controller;
 
+import com.huatuo.order.VO.ResultVO;
 import com.huatuo.order.converter.OrderForm2OrderDTOConverter;
 import com.huatuo.order.dto.OrderDTO;
 import com.huatuo.order.enums.ResultEnum;
 import com.huatuo.order.exception.OrderException;
 import com.huatuo.order.form.OrderForm;
+import com.huatuo.order.service.OrderService;
+import com.huatuo.order.utils.ResultVOUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,26 +17,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/order")
 @Slf4j
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
+
     @PostMapping(value = "/create")
-    public void create(@Valid OrderForm orderForm, BindingResult bindingResult) {
+    public ResultVO<Map<String, String>> create(@Valid OrderForm orderForm, BindingResult bindingResult) {
         //处理表单错误
         if (bindingResult.hasErrors()) {
             log.error("【创建订单】参数不正确，orderForm={}", orderForm);
-            throw new OrderException(ResultEnum.PARAM_ERROR.getCode(),bindingResult.getFieldError().getDefaultMessage());
+            throw new OrderException(ResultEnum.PARAM_ERROR.getCode(), bindingResult.getFieldError().getDefaultMessage());
         }
 
         //OrderForm -> OrderDTO
 
         OrderDTO orderDTO = OrderForm2OrderDTOConverter.convert(orderForm);
         if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())) {
-
+            log.error("【创建订单】购物车信息为空");
+            throw new OrderException(ResultEnum.CART_EMPTY);
         }
+
+        OrderDTO result = orderService.create(orderDTO);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", result.getOrderId());
+        return ResultVOUtil.success(map);
     }
 
 }
